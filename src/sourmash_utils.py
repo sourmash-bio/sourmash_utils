eea623eeb591eed3b601ec0c9859c29e36c88036
C. Titus Brown <titus@idyll.org>
2024-06-06 06:42:40 -0700

    add FracMinHash class (#1)

--- src/sourmash_utils.py
+++ src/sourmash_utils.py
@@ -1,1 +1,36 @@
-##
+"Python utilities for sourmash plugins and scripts."
+import sourmash
+
+__all__ = ['FracMinHash']
+
+class FracMinHash(sourmash.MinHash):
+    def __init__(self, *,
+                 ksize=31,
+                 scaled=1000,
+                 moltype='DNA',
+                 track_abundance=False,
+                 **kwargs):
+
+        kwargs = dict(kwargs)
+        assert 'is_protein' not in kwargs
+        assert 'dayhoff' not in kwargs
+        assert 'hp' not in kwargs
+
+        if moltype == 'DNA':
+            kwargs['is_protein'] = False
+            kwargs['dayhoff'] = False
+            kwargs['hp'] = False
+        elif moltype == 'protein':
+            kwargs['is_protein'] = True
+        elif moltype == 'dayhoff':
+            kwargs['dayhoff'] = True
+        elif moltype == 'hp':
+            kwargs['hp'] = True
+        else:
+            raise ValueError(f"unknown moltype: '{moltype}'")
+
+        super().__init__(n=0,
+                         ksize=ksize,
+                         scaled=scaled,
+                         track_abundance=track_abundance,
+                         **kwargs)
--- tests/test_sourmash_utils.py
+++ tests/test_sourmash_utils.py
@@ -1,5 +1,45 @@
-import sourmash_utils
+from sourmash_utils import *
 
 
 def test_basic():
     pass
+
+
+def test_minhash_create_dna():
+    # DNA moltype works
+    mh = FracMinHash(ksize=31, scaled=1000, moltype='DNA')
+    assert mh.moltype == 'DNA'
+    assert mh.scaled == 1000
+    assert mh.ksize == 31
+
+
+def test_minhash_create_dna_default():
+    # DNA minhashes are default
+    mh = FracMinHash(ksize=31, scaled=1000)
+    assert mh.moltype == 'DNA'
+    assert mh.scaled == 1000
+    assert mh.ksize == 31
+
+
+def test_minhash_create_protein():
+    # test moltype protein
+    mh = FracMinHash(ksize=7, scaled=100, moltype='protein')
+    assert mh.moltype == 'protein'
+    assert mh.scaled == 100
+    assert mh.ksize == 7
+
+
+def test_minhash_create_dayhoff():
+    # test moltype dayhoff
+    mh = FracMinHash(ksize=7, scaled=100, moltype='dayhoff')
+    assert mh.moltype == 'dayhoff'
+    assert mh.scaled == 100
+    assert mh.ksize == 7
+
+
+def test_minhash_create_hp():
+    # test moltype hp
+    mh = FracMinHash(ksize=7, scaled=100, moltype='hp')
+    assert mh.moltype == 'hp'
+    assert mh.scaled == 100
+    assert mh.ksize == 7
