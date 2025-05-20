29ea607ca0eaf2f00e0ae7386a0bfd326fa56d52
C. Titus Brown <titus@idyll.org>
2025-05-20 09:59:19 -0700

    MRG: misc upgrades

--- src/sourmash_utils.py
+++ src/sourmash_utils.py
@@ -3,8 +3,22 @@ import sourmash
 from sourmash import sourmash_args
 from sourmash.cli import utils as sourmash_cli
 
-__all__ = ['FracMinHash', 'add_standard_minhash_args']
+from enum import Enum
 
+__all__ = ['FracMinHash',
+           'add_standard_minhash_args']
+           
+
+DEFAULTS = dict(
+    DNA=dict(ksize=31, scaled=1000),
+    protein=dict(ksize=10, scaled=200),
+    dayhoff=dict(ksize=16, scaled=200),
+    hp=dict(ksize=42, scaled=200),
+    skipm1n3=dict(ksize=21, scaled=1000),
+    skipm2n3=dict(ksize=21, scaled=1000),
+)
+    
+           
 class FracMinHash(sourmash.MinHash):
     "An updated MinHash class with nicer constructor arguments."
     def __init__(self, *,
@@ -39,6 +53,10 @@ class FracMinHash(sourmash.MinHash):
             kwargs['dayhoff'] = True
         elif moltype == 'hp':
             kwargs['hp'] = True
+        elif moltype == 'skipm1n3':
+            kwargs['skipm1n3'] = True
+        elif moltype == 'skipm2n3':
+            kwargs['skipm2n3'] = True
         else:
             raise ValueError(f"unknown moltype: '{moltype}'")
 
@@ -54,15 +72,19 @@ class FracMinHash(sourmash.MinHash):
 
 def add_standard_minhash_args(parser):
     sourmash_cli.add_construct_moltype_args(parser)
-    sourmash_cli.add_ksize_arg(parser, default=31)
-    sourmash_cli.add_scaled_arg(parser, default=1000)
+    sourmash_cli.add_ksize_arg(parser)
+    sourmash_cli.add_scaled_arg(parser)
 
 
 def create_minhash_from_args(args, *, track_abundance=False, **defaults):
     default_moltype = defaults.get('moltype')
     moltype = sourmash_args.calculate_moltype(args, default=default_moltype)
-    ksize = args.ksize or defaults.get('ksize', 31)
-    scaled = args.scaled or defaults.get('scaled', 1000)
+    ksize = args.ksize or defaults.get('ksize')
+    if not ksize:
+        ksize = DEFAULTS[moltype]['ksize']
+    scaled = args.scaled or defaults.get('scaled')
+    if not scaled:
+        scaled = DEFAULTS[moltype]['scaled']
 
     return FracMinHash(moltype=moltype,
                        ksize=ksize,
@@ -71,8 +93,8 @@ def create_minhash_from_args(args, *, track_abundance=False, **defaults):
 
 
 def load_index_and_select(filename, minhash_obj, *, raise_on_empty=True):
-    """Load a sourmash Index object from filename, select sketches compatible
-    with minhash_obj.
+    """Load a sourmash Index object from filename,
+    selecting sketches compatible with minhash_obj.
     """
     idx = sourmash.load_file_as_index(filename)
     idx = idx.select(ksize=minhash_obj.ksize,
