c198a0d51f8434d1c7555787dc3b705d4db2b556
C. Titus Brown <titus@idyll.org>
2025-05-20 10:05:58 -0700

    MRG: misc upgrades (#7)

--- tests/test_sourmash_utils.py
+++ tests/test_sourmash_utils.py
@@ -43,3 +43,19 @@ def test_minhash_create_hp():
     assert mh.moltype == 'hp'
     assert mh.scaled == 100
     assert mh.ksize == 7
+
+
+def test_minhash_create_skipm1n3():
+    # test moltype skipm1n3
+    mh = FracMinHash(ksize=21, scaled=1000, moltype='skipm1n3')
+    assert mh.moltype == 'skipm1n3'
+    assert mh.scaled == 1000
+    assert mh.ksize == 21
+
+
+def test_minhash_create_skipm2n3():
+    # test moltype skipm1n3
+    mh = FracMinHash(ksize=21, scaled=1000, moltype='skipm2n3')
+    assert mh.moltype == 'skipm2n3'
+    assert mh.scaled == 1000
+    assert mh.ksize == 21
